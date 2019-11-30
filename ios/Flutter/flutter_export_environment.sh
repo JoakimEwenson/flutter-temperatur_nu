a5b668695c78373931668b6906d382e83f5b7e43
Joakim Ewenson <joakim@ewenson.se>
2019-11-30 13:43:56 +0100

    WIP

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\joaki\Flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\joaki\OneDrive\Kod\Flutter\flutter-temperatur_nu"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/joakim/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/joakim/Library/Mobile Documents/com~apple~CloudDocs/Kod/Flutter/temperatur_nu"
+export "FLUTTER_TARGET=/Users/joakim/Library/Mobile Documents/com~apple~CloudDocs/Kod/Flutter/temperatur_nu/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\joaki\Flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/joakim/Applications/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
