f703655d1fa3291bd7c65099b4bec1a467e01033
Joakim Ewenson <34868402+JoakimEwenson@users.noreply.github.com>
2020-01-30 14:32:14 +0100

    Drawer-changes

--- lib/views/drawer.dart
+++ lib/views/drawer.dart
@@ -8,20 +8,28 @@ class AppDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           Container(
+            color: Theme.of(context).primaryColor,
             height: 135,
             child: DrawerHeader(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.ac_unit, ),
-                    title: Text('temperatur.nu', style: Theme.of(context).textTheme.title, ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: ExactAssetImage('icon/temperatur_nu.png'),
+                    fit: BoxFit.contain
                   ),
-                ],
+                ),
               ),
+          //     child: Column(
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: <Widget>[
+          //         ListTile(
+          //           leading: Icon(Icons.ac_unit, ),
+          //           title: Text('temperatur.nu', style: Theme.of(context).textTheme.title, ),
+          //         ),
+          //       ],
+          //     ),
             ),
           ),
-          
           ListTile(
             leading: Icon(Icons.home),
             title: Text('Startsida'),
