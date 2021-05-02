0c312b997cc3ef4329f3270b5ae4f0caf7e443c6
Joakim Ewenson <joakim@ewenson.se>
2021-05-02 18:09:10 +0200

    Added themes file

---
+++ lib/views/components/theme.dart
@@ -0,0 +1,8 @@
+// Color palette
+import 'package:flutter/material.dart';
+
+Color imperialred = Color.fromRGBO(230, 57, 70, 1);
+Color honeydew = Color.fromRGBO(241, 250, 238, 1);
+Color powderblue = Color.fromRGBO(168, 218, 220, 1);
+Color celadonblue = Color.fromRGBO(69, 123, 157, 1);
+Color prussianblue = Color.fromRGBO(29, 53, 87, 1);
